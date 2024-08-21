34d4f2099ad187776286ef0ac9b95bd170f8d70a
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-08-21 23:51:49 +0200

    0x02-Minimumum_operatios

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module contains the minOperations function.
+"""
+
+def minOperations(n):
+    """
+    Calculates the minimum number of operations needed to get exactly n 'H' characters in the file.
+    
+    :param n: The target number of 'H' characters.
+    :return: The minimum number of operations or 0 if n is impossible to achieve.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,9 @@
+# 0x02. Minimum Operations
+
+## Task: Minimum Operations
+
+Write a Python function `minOperations(n)` that calculates the minimum number of operations needed to result in exactly `n` characters in a file, starting with a single character `H`.
+
+### Prototype
+```python
+def minOperations(n):
