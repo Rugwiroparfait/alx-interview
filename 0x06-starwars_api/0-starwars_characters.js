6f601e80588d3d85f469a8d60aee9caa8ef5eb41
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-09-19 22:49:10 +0200

    use of axios instead of request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,31 @@
 #!/usr/bin/node
 
-const request = require('request');
+const axios = require('axios');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 if (!movieId) {
-	console.log('Please provide a Movie ID');
-	process.exit(1);
+  console.log('Please provide a Movie ID');
+  process.exit(1);
 }
 
-request(apiUrl, function (error, response, body) {
-	if (error) {
-		console.error('Error:', error);
-	} else if (response.statusCode === 200) {
-		const filmData = JSON.parse(body);
-		const characters = filmData.characters;
+// Fetch the movie details from the API
+axios.get(apiUrl)
+  .then(response => {
+    const characters = response.data.characters;
 
-		characters.forEach((characterUrl) => {
-			request(characterUrl, function (error, response, body) {
-				if (!error && response.statusCode === 200) {
-					const characterData = JSON.parse(body);
-					console.log(characterData.name);
-				}
-			});
-		});
-	} else {
-		console.log(`Failed to retrieve movie: ${response.statusCode}`);
-	}
-});
+    // Fetch and print each character's name
+    characters.forEach((characterUrl) => {
+      axios.get(characterUrl)
+        .then(response => {
+          console.log(response.data.name);
+        })
+        .catch(error => {
+          console.error('Error fetching character:', error);
+        });
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching movie:', error);
+  });
+
